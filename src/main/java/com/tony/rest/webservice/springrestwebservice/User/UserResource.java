d325c7b615c13213587ac93125f825ba383903b2
kc-mr70ny <MR70NYAZaz09$$>
2021-03-11 15:07:14 -0600

    Initial commit

---
+++ src/main/java/com/tony/rest/webservice/springrestwebservice/User/UserResource.java
@@ -0,0 +1,68 @@
+package com.tony.rest.webservice.springrestwebservice.User;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private  UserDaoService service;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException("id-"+id);
+        }
+        EntityModel<User> resource = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+        return resource;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity CreateUser(@Valid @RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteUserById(id);
+        if(user == null){
+            throw new UserNotFoundException("id-"+id);
+        }
+    }
+
+    @GetMapping(path="/goodMorning-internationalized")
+    public String goodMorningInternationalized(){
+        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
+    }
+
+}
---
+++ src/test/java/com/tony/rest/webservice/springrestwebservice/SpringRestWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.rest.webservice.springrestwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
